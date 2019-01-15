da06026d4708584bbadb22bec0aed41bb1d1103a
Nikita Kuvykin <arrayofchar@inbox.ru>
2019-01-15 13:16:04 +0500

    - add webpack babel-loader - change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
   entry: './dev/js/building/script.js',
   output: {
     path: path.resolve(__dirname, './dev/js/'),
-    filename: 'script.js'
-  }
+    filename: 'script.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ],
+  },
 };
